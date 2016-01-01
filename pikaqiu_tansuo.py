a87e0615235bb7eb1d51e9c307eab9cc2f708496
sheissby <sheissby@sina.com>
2016-01-01 14:52:35 +0800

    增加判断是否有贼

--- pikaqiu_tansuo.py
+++ pikaqiu_tansuo.py
@@ -1,5 +1,10 @@
 # encoding:GBK
-import httplib, urllib, urllib2, re, time, json
+import httplib
+import json
+import StringIO
+import gzip
+import time
+
 
 def con(uid):
     header1 = {'Host': 'master.xiaomi.mysticalcard.com', 'Cookie': '_sid=d3kv2cgc086bs71ujmg746qqd3',
@@ -28,7 +33,7 @@ def con(uid):
             con_status = y.get('status', 0)
         else:
             con_status = 0
- #   print id1[0], 'con success'
+    # print id1[0], 'con success'
     ppsign = y.get('data', 0).get('uinfo', 0).get('ppsign', 0)
     sign = y.get('data', 0).get('uinfo', 0).get('sign', 0)
     times = y.get('data', 0).get('uinfo', 0).get('time', 0)
@@ -70,10 +75,68 @@ def con_log(*id1):
             con_log_status = y.get('status', 0)
         else:
             con_log_status = 0
-#    print id1[0], 'con_log success'
+    # print id1[0], 'con_log success'
     conn.close()
 
 
+# 得到盗贼信息
+def GetThieves(*id1):
+    con_log(*id1)
+    header1 = {'Host': 's2.xiaomi.mysticalcard.com', 'Cookie': '_sid=d3kv2cgc086bs71ujmg746qqd3',
+               'Accept': 'text/xml, application/xml, application/xhtml+xml, text/html;q=0.9, text/plain;q=0.8, '
+                         'text/css, image/png, image/jpeg, image/gif;q=0.8, application/x-shockwave-flash, video/mp4;'
+                         'q=0.9, flv-application/octet-stream;q=0.8, video/x-flv;q=0.7, audio/mp4, application'
+                         '/futuresplash, */*;q=0.5',
+               'User-Agent': 'Mozilla/5.0 (Android; U; zh-CN) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/18.0',
+               'x-flash-version': '16,0,0,276',
+               'Connection': 'Keep-Alive', 'Cache-Control': 'no-cache', 'Referer': 'app:/assets/CardMain.swf',
+               'Content-Type': 'application/x-www-form-urlencoded'
+               }
+    param0 = ""
+    conn = httplib.HTTPConnection("s2.xiaomi.mysticalcard.com")
+    GetThievesStatus = 0
+    while GetThievesStatus == 0:
+        conn.request("POST",
+                     "/arena.php?do=GetThieves&v=8313&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.7.1&pvb=2015-09-25%2017%3A07%3A26&platformtype=1",
+                     param0, header1)
+        returnstr = conn.getresponse()
+        responseheader = returnstr.getheaders()
+        isgzipped = responseheader[0]
+        if isgzipped[1] == 'gzip':
+            compresseddata = returnstr.read()
+            compressedstream = StringIO.StringIO(compresseddata)
+            gzipper = gzip.GzipFile(fileobj=compressedstream)
+            data = gzipper.read()
+            y = json.loads(data)
+            GetThievesStatus = y.get('status', 0)
+        else:
+            data = returnstr.read()
+            y = json.loads(data)
+            GetThievesStatus = y.get('status', 0)
+    return y
+    conn.close()
+
+
+# 返回0表示没有贼，可以探索；返回1表示有贼，不需探索
+def ExistThief(thievesinfo):
+    Thievesinfo = thievesinfo.get('data', 0).get('Thieves', 0)
+    Countdown = thievesinfo.get('data', 0).get('Countdown', 0)
+    currentuid = id1[0]
+    currentuid = currentuid.decode('GBK')
+    a = 0
+    for a in Thievesinfo:
+        thievesNickName = a.get('NickName', 0)
+        thievesstatus = a.get('Status', 0)
+        thievesfleetime = a.get('FleeTime', 0)
+        thieveshpcurrent = a.get('HPCurrent', 0)
+        # 判断有贼未死或有贼未跑，不需探索
+        if (thievesNickName == currentuid and thievesstatus == 1 and thieveshpcurrent > 0) \
+                or (thievesNickName == currentuid and thievesstatus == 0 and thievesfleetime > 0):
+            return 1, Countdown
+    return 0, Countdown
+
+
+# 进行探索
 def mapstage(*id1):
     con_log(*id1)
     header1 = {'Host': 's2.xiaomi.mysticalcard.com', 'Cookie': '_sid=d3kv2cgc086bs71ujmg746qqd3',
@@ -86,7 +149,7 @@ def mapstage(*id1):
                'Connection': 'Keep-Alive', 'Cache-Control': 'no-cache', 'Referer': 'app:/assets/CardMain.swf',
                'Content-Type': 'application/x-www-form-urlencoded'
                }
-    param0 = "MapStageDetailId=20"
+    param0 = "MapStageDetailId=50"
     conn = httplib.HTTPConnection("s2.xiaomi.mysticalcard.com")
     conn.request("POST",
                  "/mapstage.php?do=Explore&v=4581&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.7.1"
@@ -94,14 +157,15 @@ def mapstage(*id1):
                  param0, header1)
     returnstr = conn.getresponse()
     lenth = returnstr.read()
-#    print 'mapstage response:', lenth
+    # print 'mapstage response:', lenth
     y = json.loads(lenth)
-    status = y.get('status', 0)
+    mapstagestatus = y.get('status', 0)
     conn.close()
-    list = lenth, status
+    list = lenth, mapstagestatus
     return list
 
 
+# 攻击盗贼
 def thievesfight(userthievesid):
     header1 = {'Host': 's2.xiaomi.mysticalcard.com', 'Cookie': '_sid=d3kv2cgc086bs71ujmg746qqd3',
                'Accept': 'text/xml, application/xml, application/xhtml+xml, text/html;q=0.9, text/plain;q=0.8, '
@@ -124,7 +188,6 @@ def thievesfight(userthievesid):
                  param0, header1)
     conn.close()
 
-
 # 账户列表
 id = [['杰尼龟', '2014041855227765', '273122'], ['小火龙', '2014042155811563', '273419'], ['妙蛙种子', '2014052561883286', '278956'],
       ['绿毛虫', '2014061766465489', '278958'], ['大针蜂', '2014061866519659', '278984'], ['比比鸟', '2014061866519756', '278986'],
@@ -135,29 +198,41 @@ id = [['杰尼龟', '2014041855227765', '273122'], ['小火龙', '2014042155811563', '
       ['比雕', '2014061866529500', '279117'], ['雷精灵', '2014061866529554', '279119'], ['皮卡西', '2014061866529628', '279122'],
       ['水精灵', '2014061866529641', '279131'], ['火精灵', '2014061866529643', '279137'], ['胡地', '2014061866529675', '279164'],
       ['风速狗', '2014061866529735', '279165'], ['喷火龙', '2014061866529744', '279166']]
-
-
 for id1 in id:
-    lenth1 = ''
-    status1 = 1
-    while len(lenth1) < 400 and status1 == 1:
-        lenth1, status1 = mapstage(*id1)
-        if status1 == 0:  # status=0表示探索失败，跳出本次循环
-            break
-        else:
-            #  返回值长度大于400，表示有盗贼
-            if len(lenth1) > 400:
-                y = json.loads(lenth1)
-                # 获得盗贼血量
-                Type = y.get('data', 0).get('ThievesInfo', 0).get('Type', 0)
-                userthievesid = y.get('data', 0).get('ThievesInfo', 0).get('UserThievesId', 0)
-                if Type == 2:
-                    print id1[0], '出现精英盗贼！！！'
-                    # 出现精英盗贼自动攻击
-                    # thievesfight(userthievesid)
-                else:
-                    print id1[0], '出现普通盗贼'
-                    thievesfight(userthievesid)
+    thievesinfo = GetThieves(*id1)
+    thief = ExistThief(thievesinfo)
+    thievesfightCD = thief[1]    #打贼cd时间
+    if thief[0] == 1:
+        print id1[0],'不需探索'
+        continue
+    else:
+        print id1[0],'可以探索'
+        lenth1 = ''
+        status1 = 1
+        while len(lenth1) < 400 and status1 == 1:
+            lenth1, status1 = mapstage(*id1)
+            if status1 == 0:  # status=0表示探索失败，跳出本次循环
                 break
-    time.sleep(0.1)
+            else:
+                if len(lenth1) > 400:    #  返回值长度大于400，表示有盗贼
+                    y = json.loads(lenth1)
+                    # 获得盗贼类型
+                    Type = y.get('data', 0).get('ThievesInfo', 0).get('Type', 0)
+                    userthievesid = y.get('data', 0).get('ThievesInfo', 0).get('UserThievesId', 0)
+                    if Type == 2:
+                        print id1[0], '出现精英盗贼'
+                        if thievesfightCD <= 0:
+                            thievesfight(userthievesid)    # 出现精英盗贼自动攻击
+                        else:
+                            print id1[0], '打贼cd中......'
+                    else:
+                        print id1[0], '出现普通盗贼'
+                        if thievesfightCD <= 0:
+                            thievesfight(userthievesid)
+                        else:
+                            print id1[0], '打贼cd中......'
+                    break
+        time.sleep(0.1)
+
+
 raw_input('End')
