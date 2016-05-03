7ffc07ef009ce5b0e50af7e9daddaaa284dd5241
sheissby <sheissby@sina.com>
2016-05-03 09:43:17 +0800

    增加返回正常与异常标识

--- login.py
+++ login.py
@@ -22,7 +22,7 @@ def connection(url, data):
                     print '登录失败'
                     time.sleep(1)
                 else:
-                    return 1
+                    return 0, message
         except requests.ConnectionError, e:
             print e
             status = 0
@@ -31,14 +31,17 @@ def connection(url, data):
             print e
             status = 0
             time.sleep(1)
-    return jsonresponse
+    return 1, jsonresponse
 
 
 def con(uid, sessionid):
     con_data = "Udid=64%3A09%3A80%3AD3%3AF3%3A0E&plat=ANDROID%5FXIAOMI&newguide=1&IDFA=" + '&uid=' + uid + '&sessionid=' + sessionid
     url = 'http://s1.xiaomi.mysticalcard.com/mpassport.php?do=plogin&v=3337&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.7.0&pvb=2015-07-16%2017%3A02%3A55&platformtype=null'
-    jsonresponse = connection(url, con_data)
-    return jsonresponse
+    flg, jsonresponse = connection(url, con_data)
+    if flg != 0:
+        return jsonresponse
+    else:
+        print jsonresponse
 
 
 def con_log(*id1):
@@ -57,7 +60,7 @@ def con_log(*id1):
     f = '&nick=' + uid
     conlog_data = "access%5Ftoken=&plat=ANDROID%5FXIAOMI&newguide=1&Devicetoken=&Origin=xiaomi&IDFA=&Udid=64%3A09%3A80%3AD3%3AF3%3A0E" + d + e + f + c + b + a
     url = 'http://s1.xiaomi.mysticalcard.com/login.php?do=mpLogin&v=1521&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.7.0&pvb=2015-07-16%2017%3A02%3A55&platformtype=null'
-    jsonresponse = connection(url, conlog_data)
+    flg, jsonresponse = connection(url, conlog_data)
     # print id1[0], 'con_log success!'
 
 
@@ -67,18 +70,31 @@ def getAward():
     data = 'pvpNewVersion=1'
     url1 = host + '/user.php?do=AwardSalary&v=1523&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.7.1&pvb=2015-09-25%2017%3A07%3A26&platformtype=1'
     data1 = ''
-    jsonresponse = connection(url, data)
-    print id1[0], 'GetUserinfo sucess!'
-
-    jsonresponse = connection(url1, data1)
-    print id1[0], 'AwardSalaryres sucess!'
+    flg = 0
+    while flg == 0:
+        flg, jsonresponse = connection(url, data)
+        if flg != 0:
+            print id1[0], 'GetUserinfo sucess!'
+        else:
+            print id1[0], 'GetUserinfo failed!', jsonresponse
+    flg = 0
+    while flg == 0:
+        flg, jsonresponse = connection(url1, data1)
+        if flg != 0:
+            print id1[0], 'AwardSalary sucess!'
+        else:
+            print id1[0], 'AwardSalary failed!', jsonresponse
 
 def GetUserMapStages():
     url = 'http://s1.xiaomi.mysticalcard.com/mapstage.php?do=GetUserMapStages&v=7002&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.7.1&pvb=2015-09-25%2017%3A07%3A26&platformtype=1'
     data = ''
-    # �õ����ֵ�����Ϣ
-    jsonresponse = connection(url, data)
-    data = jsonresponse.get('data', 0)
+    flg = 0
+    while flg == 0:
+        flg, jsonresponse = connection(url, data)
+        if flg != 0:
+            data = jsonresponse.get('data', 0)
+        else:
+            print id1[0], 'GetUserMapStages failed!', jsonresponse
     arr = []
     for i in data:
         counterattacktime = data.get(i, 0).get('CounterAttackTime', 0)
@@ -92,7 +108,7 @@ def EditUserMapStages(arr):
     url = 'http://s1.xiaomi.mysticalcard.com/mapstage.php?do=EditUserMapStages&v=7003&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.7.1&pvb=2015-09-25%2017%3A07%3A26&platformtype=1'
     for i in arr:
         data = 'isManual=0&MapStageDetailId=' + i
-        jsonresponse = connection(url, data)
+        flg, jsonresponse = connection(url, data)
 
 
 def Worship(*id1):
@@ -100,7 +116,7 @@ def Worship(*id1):
     url = 'http://s1.xiaomi.mysticalcard.com/worship.php?do=Worship&v=6132&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.7.1&pvb=2015-09-25%2017%3A07%3A26&platformtype=1'
     for type in ['1', '2', '3']:
         data = 'Type=' + type
-        jsonresponse = connection(url, data)
+        flg, jsonresponse = connection(url, data)
 
 id = loginid()
 
