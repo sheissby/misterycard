fab4597e9e84f71994785a64c4aaeda85bb60775
sheissby <sheissby@sina.com>
2016-09-08 21:00:15 +0800

    watch suspension info

---
+++ suspension.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+import requests
+import time
+import json
+
+
+def get_suspension_info():
+    date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    url = 'http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx?' \
+          'type=FD&sty=SRB&st=0&sr=-1&p=1&ps=1000&js=var%20veuNVpqs={pages:(pc),data:[(x)]}&mkt=1&fd=' + date + '&rt=49093586'
+    r = requests.get(url)
+    html = r.content
+    data = html.split('=')[1]
+    data = data.replace('pages', '"pages"')
+    data = data.replace('data', '"data"')
+    contents = json.loads(data)['data']
+    return contents
+
+def getFileinfo(filename):
+    stockid_or_name = ''
+    # 打开文件
+    f = open(filename, 'r')
+    for line in f:
+        # 拼装内容
+        if not stockid_or_name:
+            stockid_or_name = line
+        else:
+            stockid_or_name = stockid_or_name + ',' + line
+    f.close()
+    stockid_or_name = stockid_or_name.replace('\n', '')    # 去掉'\n'
+    stockid_or_name_as_list = stockid_or_name.split(',')   # 逗号分隔成list
+    return stockid_or_name_as_list
+
+def findinfo(stockid_or_name_as_list, suspension_info):
+    # 在已抓取信息中循环查找
+    for stockidorname in stockid_or_name_as_list:  # 关注股票list
+        for suspension in suspension_info:   # 已抓取股票list
+            suspension = suspension.split(',')
+            if stockidorname == suspension[0].encode('utf-8') or stockidorname == suspension[1].encode('utf-8'):
+                print suspension[0],suspension[1],suspension[2], suspension[8],suspension[4],suspension[5]
+
+
+if __name__ == '__main__':
+    suspension_info = get_suspension_info()
+    stockid_or_name_as_list = getFileinfo('stock.txt')
+    findinfo(stockid_or_name_as_list, suspension_info)
