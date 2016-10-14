761ea11b47d1e9a4929d2a254f23b2cd24d52929
by <sheissby@sina.com>
2016-10-14 17:57:48 +0800

    add note

--- misterycard/con_log.py
+++ misterycard/con_log.py
@@ -11,11 +11,13 @@ config.read('init.conf')
 host = config.get('Host', 'host')
 
 
+# 成功返回1和response信息，失败返回0和失败信息
 def connection(url, data):
     status = 0
     while status == 0:
         try:
             response = requests.post(url, data=data, headers=header)
+            print response.content
             jsonresponse = json.loads(response.content)
             status = jsonresponse.get('status', 0)
             if status == 0:
@@ -33,6 +35,10 @@ def connection(url, data):
             print e
             status = 0
             time.sleep(1)
+        # except Exception, e:
+        #     print e
+        #     status = 0
+        #     time.sleep(1)
     return 1, jsonresponse
 
 
@@ -43,10 +49,10 @@ def con(uid, sessionid):
     return jsonresponse
 
 
-def con_log(*id1):
-    uid = id1[1]
-    Muid = id1[2]
-    sessionid = id1[3]
+def con_log(id):
+    uid = id[1]
+    Muid = id[2]
+    sessionid = id[3]
     y = con(uid, sessionid)
     ppsign = y.get('data', 0).get('uinfo', 0).get('ppsign', 0)
     sign = y.get('data', 0).get('uinfo', 0).get('sign', 0)
@@ -62,20 +68,20 @@ def con_log(*id1):
     flag, jsonresponse = connection(url, conlog_data)
     # print id1[0], jsonresponse
 
-def getmapid():
-    MapStageId =[]    #3星地图id集合
-    MapStageIdAll =[] #所有已开地图集合
-    flg = 0
-    url = host + '/mapstage.php?do=GetUserMapStages&v=3885&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.8.1&pvb=2016-04-12%2009%3A53%3A52&platformtype=1'
-    jsonresponse = connection(url, data='')
-    mapinfoall = jsonresponse.get('data')
-    print mapinfoall
-
-def getwanneng():
-    url = 'http://s1.xiaomi.mysticalcard.com/cardchip.php?do=CompositeChip&v=6413&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.8.1&pvb=2016-04-12%2009%3A53%3A52&platformtype=1'
-    data = 'ChipId=6003&IsSuper=0'
-    json = connection(url, data)
-    print json
+# def getmapid():
+#     MapStageId =[]    #3星地图id集合
+#     MapStageIdAll =[] #所有已开地图集合
+#     flg = 0
+#     url = host + '/mapstage.php?do=GetUserMapStages&v=3885&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.8.1&pvb=2016-04-12%2009%3A53%3A52&platformtype=1'
+#     jsonresponse = connection(url, data='')
+#     mapinfoall = jsonresponse.get('data')
+#     print mapinfoall
+#
+# def getwanneng():
+#     url = 'http://s1.xiaomi.mysticalcard.com/cardchip.php?do=CompositeChip&v=6413&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.8.1&pvb=2016-04-12%2009%3A53%3A52&platformtype=1'
+#     data = 'ChipId=6003&IsSuper=0'
+#     json = connection(url, data)
+#     print json
 
 
 if __name__ == '__main__':
