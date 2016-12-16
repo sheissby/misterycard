7e0faced13236cb522545541654a1cf33c5cf6ca
by <sheissby@sina.com>
2016-12-16 16:29:56 +0800

    del id

--- newmisterycard/GetCard.py
+++ newmisterycard/GetCard.py
@@ -24,7 +24,7 @@ def getCardUntilGet(id1, targetid):
 
 if __name__ == '__main__':
     ids = [
-        ['Dsus4', '2013122514488598', '290176', 'LnQ6qpKCyY95nIrg']
+
           ]
     targetid = input('please enter targetid:')
     for id in ids:
