21ac21a0cd5b6ab88b7458291b6eba0248d5764d
by <sheissby@sina.com>
2017-05-08 16:46:58 +0800

    add

--- other/crawl_analysis.py
+++ other/crawl_analysis.py
@@ -3,22 +3,23 @@ from crawl_lagou import get_response as lg_get_response
 from crawl_zhilian import get_response as zl_get_response
 
 
-def data(crawler, site):
-    position_data = crawler
+def clawl(spider, site):
+    position_data = spider
     data = []
     for position in position_data:
         data.append(position)
 
-    salary = []
-    sum = 0
-    for info in data:
-        salary.append(info['salary']['avg'])
-    for i in salary:
-        sum += i
-    avg_salary = sum / len(salary)
-    print '%s抓取%d个职位，平均薪资为%d' % (site, len(salary), avg_salary)
+    for level in ('min', 'avg', 'max'):
+        salary = []
+        sum = 0
+        for info in data:
+            salary.append(info['salary'][level])
+        for i in salary:
+            sum += i
+        avg_salary = sum / len(salary)
+        print '%s抓取%d个职位，平均%s薪资为%d' % (site, len(salary), level, avg_salary)
 
 
 if __name__ == '__main__':
-    data(lg_get_response, '拉勾')
-    data(zl_get_response, '智联')
+    clawl(lg_get_response(), '拉勾')
+    clawl(zl_get_response(), '智联')
