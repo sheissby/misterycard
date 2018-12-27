9b5e70e6b5b64ec6c17ccd74a20b33042dee13c1
sheiss <sheissby@sina.com>
2018-12-27 19:28:54 +0800

    修改取歌手名的bug

--- other/cue.py
+++ other/cue.py
@@ -8,14 +8,14 @@ filelist = []
 def GetFilesAndPath(file_dir):
     for root, dirs, files in os.walk(file_dir):
         for i in files:
-            if i.split('.')[-1].lower() in ('ape', 'wav'):
+            if i.split('.')[-1].lower() in ('ape', 'wav', 'flac'):
                 filelist.append(i)
     return root, filelist
 
 
 def writecue(path, files):
     # 根据文件夹名（“歌手 - 专辑”）提取歌手和专辑名
-    performer = path.split('-')[0].split('\\')[1].rstrip()
+    performer = path.split('-')[0].split('\\')[-1].rstrip()
     title = path.split('-')[1].lstrip()
     filename = path + '\\' + title + '.cue'
     with codecs.open(filename, 'w', encoding='gbk') as f:
