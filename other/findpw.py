d885738f587e8c94908726c6d4c123c704312768
sheiss <sheissby@sina.com>
2020-03-03 13:33:54 +0800

    改为py3

--- other/findpw.py
+++ other/findpw.py
@@ -1,13 +1,14 @@
 # encoding: utf-8
 import base64
-from Crypto.Cipher import AES
+# from Crypto.Cipher import AES
+from Cryptodome.Cipher import AES
 import win32clipboard as w
 import win32con
 
 
 def encrypt():
     # 密文
-    password = 'baiyang000000000'
+    password = 'baiyang000000000'.encode('utf-8')
     # 秘钥
     key = input("Enter key:")
     if key:
@@ -18,9 +19,9 @@ def encrypt():
         if add:
             key = key + ('\0' * (lenth - add))
         # 使用AES加密，密文转base64
-        encryptor = AES.new(key, AES.MODE_CBC, b'0000000000000000')
+        encryptor = AES.new(key.encode('utf-8'), AES.MODE_CBC, b'0000000000000000')
         ciphertext = base64.b64encode(encryptor.encrypt(password))
-        ciphertext = ciphertext.replace('/', '')
+        ciphertext = ciphertext.decode('utf-8').replace('/', '')
         # 截取特定位数
         ciphertext = ciphertext[0: 8]
         print(ciphertext)
@@ -31,10 +32,11 @@ def encrypt():
 def setText(ciphertext):
     w.OpenClipboard()
     w.EmptyClipboard()
-    w.SetClipboardData(win32con.CF_TEXT, ciphertext)
+    w.SetClipboardData(win32con.CF_TEXT, ciphertext.encode('utf-8'))
     w.CloseClipboard()
 
 
 if __name__ == '__main__':
-    setText(encrypt())
-    raw_input()
+    while True:
+        setText(encrypt())
+        input()
